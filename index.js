601dfafb9df854ed2adaab9d8d8a009c02651ee4
joshshark <53581330+joshshark@users.noreply.github.com>
2022-03-22 21:41:54 -0400

    added one round.  can only get first alert

--- index.js
+++ index.js
@@ -12,33 +12,41 @@ function computerPlay() {
 
 const computerSelection = computerPlay();
 
-function oneRound(playerSelection, computerSelection) {
-  let choices = ["rock" || "scissors" || "paper"];
-  let input = playerSelection.toLowerCase();
+const playerPlay = prompt("Rock, Scissors, Paper?", "🤘 ✂ 🧻"); //Player inputs a selection via prompt
 
-  if (playerSelection != choices)
-    return 'Please type "rock", "scissors", or "paper"';
-  else if (
+console.log(computerPlay);
+console.log(playerPlay);
+
+function oneRound(playerSelection, computerSelection) {
+  playerSelection = playerPlay;
+  const input = playerSelection.toLowerCase();
+  const choices = ["rock" || "scissors" || "paper"];
+
+  if (input !== choices) {
+    return alert('Please type "rock", "scissors", or "paper"');
+  } else if (
     (playerSelection,
     computerSelection === ["rock", "scissors"] || ["scissors", "paper"] || [
         "paper",
         "rock",
       ])
-  )
-    return "Good job!  You beat the computer!";
-  else if (
+  ) {
+    return alert("Good job!  You beat the computer!");
+  } else if (
     (playerSelection,
     computerSelection === ["rock", "paper"] || ["scissors", "rock"] || [
         "paper",
         "scissors",
       ])
-  )
-    return "You lost!  Try again!";
-  else
+  ) {
+    return alert("You lost!  Try again!");
+  } else
     playerSelection,
       computerSelection === ["rock", "rock"] || ["scissors", "scissors"] || [
           "paper",
-          "papers",
+          "paper",
         ];
-  return "You tied! Try again!";
+  return alert("You tied! Try again!");
 }
+
+console.log(oneRound("", computerSelection));
